ce7de77ad18aa37352e8355289cc3cb7c985c68b
nirvaanrohira <nirvaanr@student.ubc.ca>
2021-10-14 02:22:41 -0700

    Task 5 almost done just list output remaining Task 4 completed
    Added Tests for all classes
    
    Fixed Missed Branch

---
+++ src/main/ui/PatientApp.java
@@ -0,0 +1,81 @@
+package ui;
+//Patient Application
+
+import model.PatientData;
+import java.util.Scanner;
+
+public class PatientApp {
+    private Scanner input;
+    private PatientData patient;
+
+    public PatientApp() {
+        runPatient();
+    }
+
+    //MODIFIES: this
+    //EFFECTS: Takes and processes user inputs
+    private void runPatient() {
+        boolean keepGoing = true;
+        String command = null;
+
+        init();
+
+        while (keepGoing) {
+            displayMenu();
+            command = input.next();
+            command = command.toLowerCase();
+
+            if (command.equals("q")) {
+                keepGoing = false;
+            } else {
+                processCommand(command);
+            }
+        }
+        System.out.println("\nGoodbye!");
+
+    }
+
+    // EFFECTS: displays menu of options to user
+    private void displayMenu() {
+        System.out.println("\nSelect from:");
+        System.out.println("\ta -> add person");
+        System.out.println("\tq -> quit");
+
+    }
+
+    private void init() {
+        //patient = new PatientData();
+        //Inititalse patient is an issue
+        input = new Scanner(System.in);
+        input.useDelimiter("\n");
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes user command
+    private void processCommand(String command) {
+        if (command.equals("a")) {
+            doAddPerson();
+        } else {
+            System.out.println("Invalid Selection...");
+        }
+    }
+
+
+    private void doAddPerson() {
+        System.out.print("Add the Patient's Name: ");
+        String name = input.next();
+        System.out.print("Add the Patient's Age: ");
+        int age = Integer.parseInt(input.next());
+        System.out.print("Add the Patient's Duration of Stay: ");
+        int stayTime = Integer.parseInt(input.next());
+        System.out.print("Add the Patient's Treatment Received: ");
+        String treatmentRecieved = input.next();
+        System.out.print("Add the Patient's Condition: ");
+        String condition = input.next();
+        System.out.print("Add the Patient's Total Cost: ");
+        int cost = Integer.parseInt(input.next());
+        PatientData buildlist = new PatientData(name, age, stayTime, treatmentRecieved, condition, cost);
+
+    }
+
+}
