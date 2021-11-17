225d7b0a4f37192e22177b027df4c32588c65503
nirvaanrohira <nirvaanr@student.ubc.ca>
2021-11-16 23:38:30 -0800

    I HATE YOU CHECKSTYLE!!! Kinda done kinda not Started GUI Swing CLass (Not sure about exceptions in it though Phase 2 complete Phase 2 almost complete Write test error casuing changes to read and write data files Added Phase 2 user stories
    Task 5 done
    Task 4 completed
    
    Added Tests for all classes
    
    Fixed Missed Branch
    
    Added New Patient App Functions

--- src/main/ui/GuiSwingClass.java
+++ src/main/ui/GuiSwingClass.java
@@ -69,7 +69,7 @@ public class GuiSwingClass extends JPanel {
 
     // EFFECTS: Create a panel that uses the BoxLayout and adds all the panel functionalities.
     public void createBoxPanel() {
-        //shorten
+        //shorten method
         JScrollPane listScrollPane = new JScrollPane(list);
         JPanel buttonPane = new JPanel();
         buttonPane.setLayout(new BoxLayout(buttonPane,
